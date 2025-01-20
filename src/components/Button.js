650f4de87d6f00402fc1013bf90acf6480e9dbb4
Your Name <you@example.com>
2025-01-20 10:54:38 +0100

    commit 1

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../styles/Button.module.css'
+import '../styles/Style.css'
+const Button = () => {
+  return (
+    <>
+        <button className={styles.error}>Button</button>
+        <button className='error'>button</button>
+
+    </>
+  )
+}
+
+export default Button
